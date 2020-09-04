8df5887c57e1f348a9f62a71d2dfcd03550c6aa3
jjp6315 <jjp6315@psu.edu>
2020-09-04 03:24:24 +0000

    d

--- main.py
+++ main.py
@@ -2,14 +2,12 @@ temperature = float(input("Enter temperature: "))
 
 unit = input("Enter unit in F/f or C/c: ")
 if unit == "F" or unit == "f":
-  print("Converting Fahrenheit to Celsius.")
   
   c = (temperature - 32) * 5/9
  
   print(f"{temperature}\N{degree sign} in Fahrenheit is equivalent to {c}\N{degree sign} Celsius.")
 
 elif unit == "C" or unit == "c":
-  print("Converting Celsius to Fahrenheit.")
  
   f = temperature * 9/5 + 32
   
